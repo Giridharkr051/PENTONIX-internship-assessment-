81b5a2641917652472e4dede0bc3810ac4d60c79
Giridhar k r <84076569+Giridharkr051@users.noreply.github.com>
2023-09-04 15:46:09 +0530

    Create creating_in_memory_data_base.py

---
+++ question2/creating_in_memory_data_base.py
@@ -0,0 +1,20 @@
+
+# we can create in memory database using the dictionary in python
+# Create an in-memory database for departments
+departments = {
+    10: 'Admin',
+    20: 'Accounts',
+    30: 'Sales',
+    40: 'Marketing',
+    50: 'Purchasing'
+}
+
+# Create an in-memory database for employees
+employees = [
+    {"ENO": 1, "ENAME": "Amal", "DNO": 10, "SALARY": 30000},
+    {"ENO": 2, "ENAME": "Shyamal", "DNO": 30, "SALARY": 50000},
+    {"ENO": 3, "ENAME": "Kamal", "DNO": 40, "SALARY": 10000},
+    {"ENO": 4, "ENAME": "Nirmal", "DNO": 50, "SALARY": 60000},
+    {"ENO": 5, "ENAME": "Bimal", "DNO": 20, "SALARY": 40000},
+    {"ENO": 6, "ENAME": "Parimal", "DNO": 10, "SALARY": 20000}
+]
