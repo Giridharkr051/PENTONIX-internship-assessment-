3bef31f61922cdea85b55b90920e67200d88f943
Giridhar k r <84076569+Giridharkr051@users.noreply.github.com>
2023-09-04 14:51:08 +0530

    Create length_of_long_sub.py

---
+++ question-1 /length_of_long_sub.py
@@ -0,0 +1,14 @@
+
+def length_of_long_sub(s):
+    charset=set()
+    l=0
+    res=0
+    
+    for r in range(len(s)):
+        while s[r] in charset:
+            charset.remove(s[l])
+            l+=1 
+        charset.add(s[r])
+        res=max(res,r-l+1)
+    return res
+print(length_of_long_sub("abcabcdd"))
